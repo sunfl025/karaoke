16c895a17e4d9a17a1ba9c6c6af2071d41f8f312
thanhnhan16 <99702730+thanhnhan16@users.noreply.github.com>
2022-10-19 14:47:46 +0700

    nhan dep zai

--- src/main/java/GUI/Quanlidichvu.java
+++ src/main/java/GUI/Quanlidichvu.java
@@ -72,7 +72,6 @@ public class Quanlidichvu extends javax.swing.JPanel {
 
         lbl_tendichvu.setText("Tên dịch vụ : ");
 
-        btn_themdichvu.setIcon(new javax.swing.ImageIcon("img/Create.png")); // NOI18N
         btn_themdichvu.setText("Tạo mới");
         btn_themdichvu.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -80,7 +79,6 @@ public class Quanlidichvu extends javax.swing.JPanel {
             }
         });
 
-        btn_capnhatdichvu.setIcon(new javax.swing.ImageIcon("img/Edit.png")); // NOI18N
         btn_capnhatdichvu.setText("Cập nhật");
         btn_capnhatdichvu.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -88,7 +86,6 @@ public class Quanlidichvu extends javax.swing.JPanel {
             }
         });
 
-        btn_lamoidichvu.setIcon(new javax.swing.ImageIcon("img/Refresh.png")); // NOI18N
         btn_lamoidichvu.setText("Làm mới");
         btn_lamoidichvu.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -96,7 +93,6 @@ public class Quanlidichvu extends javax.swing.JPanel {
             }
         });
 
-        btn_timkiemdichvu.setIcon(new javax.swing.ImageIcon("img/Search.png")); // NOI18N
         btn_timkiemdichvu.setText("Tìm kiếm");
         btn_timkiemdichvu.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
